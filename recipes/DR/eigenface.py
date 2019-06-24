08a2359cba04a1899d2cfb1fa0c37c3bff7ee3f4
Jeremy Fix <jeremy.fix@gmail.com>
2019-06-24 07:14:29 +0200

    1st version of the algorithm :tada:

--- recipes/DR/eigenface.py
+++ recipes/DR/eigenface.py
@@ -2,7 +2,8 @@
 # Copyright 2019 (C) Jeremy Fix
 # Released under a BSD two-clauses license
 #
-# Reference:  TODO
+# Reference: M. Turk & A. Pentland (1991) Eigenfaces for Recognition.
+#            Journal of cognitive neuroscience, 3(1): 71-86.
 # Dataset : Database of faces from AT&T Laboratories Cambridge
 #           https://www.cl.cam.ac.uk/research/dtg/attarchive/facedatabase.html
 # -----------------------------------------------------------------------------
@@ -21,9 +22,29 @@ class EigenFace:
 
     def fit(self, X):
         '''
-        TODO
+        Compute the PCA by diagonalizing the Gram matrix
+        The data are supposed to be laid out in the rows of X
         '''
-        pass
+
+        # Compute the Gram matrix, i.e. the matrix of
+        # dot product of the centered samples
+        centroid = X.mean(axis = 0)
+        Xc = X - centroid
+        G = Xc @ Xc.T
+
+        # Compute the eigenvalue/eigenvector decomposition of G
+        eigvals, eigvecs = np.linalg.eigh(G)
+
+        # It might be that some eigvals < 0
+        # which theoretically cannot happen
+        eigvals[eigvals < 0] = 0
+
+
+        # TODO : handle when eigvals = 0
+        # Compute the eigenvectors of the covariance matrix
+        sig_eigvecs = (Xc.T @ eigvecs) / np.sqrt(eigvals)
+
+        return eigvals, sig_eigvecs
 
 
     def transform(self, Z):
@@ -42,11 +63,10 @@ if __name__ == '__main__':
     print('-' * 70)
 
     samples = np.load('att_faces.npy')
-    X = samples['input']
+    X, y = samples['input'], samples['label']
 
     ## Extract the 10 first principal component vectors
     n_components = 10
     eigenface = EigenFace(n_components = n_components)
     eigenface.fit(X)
-
 
